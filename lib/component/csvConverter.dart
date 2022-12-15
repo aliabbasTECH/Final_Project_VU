dff1c3044f7b6a6c0e14a04baf60f308a267ee3b
aliabbastech03 <aliabbas.tech03@gmail.com>
2022-12-15 23:02:23 +0500

    12/15/2022 add csv to json converter

--- lib/component/csvConverter.dart
+++ lib/component/csvConverter.dart
@@ -1,74 +1,87 @@
-import 'dart:io';
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// import 'dart:io';
+// import 'package:file_picker/file_picker.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 
-import '../screens/admin/adminComponent/adrawer.dart';
+// import '../screens/admin/adminComponent/adrawer.dart';
 
-class CsvToJson extends StatefulWidget {
-  const CsvToJson({super.key});
+// class CsvToJson extends StatefulWidget {
+//   const CsvToJson({super.key});
 
-  @override
-  State<CsvToJson> createState() => _CsvToJsonState();
-}
+//   @override
+//   State<CsvToJson> createState() => _CsvToJsonState();
+// }
 
-class _CsvToJsonState extends State<CsvToJson> {
-  File? csvfile;
-  dynamic users;
+// class _CsvToJsonState extends State<CsvToJson> {
+//   File? csvfile;
+//   dynamic users;
 
-  selectCSVFile() async {
-    final result = await FilePicker.platform
-        .pickFiles(type: FileType.custom, allowedExtensions: ['csv']);
+//   selectCSVFile() async {
+//     final result = await FilePicker.platform
+//         .pickFiles(type: FileType.custom, allowedExtensions: ['csv']);
 
-    if (result != null) {
-      final path = result.files.single.path!;
-      setState(() {
-        csvfile = File(path);
-        users = csvfile?.readAsStringSync();
-        var data = csvToUsersList(users);
-        print("___________________________");
-        print(data);
-        print("___________________________");
-      });
-    }
-  }
+//     if (result != null) {
+//       final path = result.files.single.path!;
+//       setState(() {
+//         csvfile = File(path);
+//         users = csvfile?.readAsStringSync();
+//         List<User> users1 = csvToUsersList(users);
+      
+//         String dataBuffer = "";
+      
+//         for (var i = 0; i < users1.length; i++) {
+//           dataBuffer += (i == 0) ? "" : ",\n";
+//           dataBuffer += users1[i].toJson();
+//         }
+  
+//         dataBuffer = "[" + dataBuffer + "]";
+  
+//         print("___________________________");
+//         print(dataBuffer);
+//         print("___________________________");
+//       });
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Manage Products")),
-      drawer: AdDrawer(),
-      body: Center(
-          child: Column(
-        children: [
-          ElevatedButton(onPressed: selectCSVFile, child: Text("CSV"))
-        ],
-      )),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Manage Products")),
+//       drawer: AdDrawer(),
+//       body: Center(
+//           child: Column(
+//         children: [
+//           ElevatedButton(onPressed: selectCSVFile, child: Text("CSV"))
+//         ],
+//       )),
+//     );
+//   }
+// }
 
-// class to create list
+// // class to create list
 
-class User {
-  String username;
-  String email;
-  String phone;
+// class User {
+//   String username;
+//   String email;
+//   String phone;
 
-  User(this.username, this.email, this.phone);
-}
+//   User(this.username, this.email, this.phone);
+//   toJson() {
+//           return '{ "username": "${username}", "email": "${email}", "phone": "${phone}" }';
+//         }
+// }
 
-List<User> csvToUsersList(String data) {
-  List<User> users = [];
+// List<User> csvToUsersList(String data) {
+//   List<User> users = [];
 
-  List<String> userin = data.split("\n");
+//   List<String> userin = data.split("\n");
 
-  for (int i = 1; i < userin.length; i++) {
-    List<String> user = userin[i].split(",");
+//   for (int i = 1; i < userin.length; i++) {
+//     List<String> user = userin[i].split(",");
 
-    users.add(User(user[0], user[1], user[2]));
-  }
+//     users.add(User(user[0], user[1], user[2]));
+//   }
 
-  return users;
-}
+//   return users;
+// }
--- lib/screens/admin/Manageproducts.dart
+++ lib/screens/admin/Manageproducts.dart
@@ -1,8 +1,6 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
-import '../../component/csvConverter.dart';
 import 'adminComponent/adrawer.dart';
 
 class ManageProducts extends StatefulWidget {
@@ -13,36 +11,125 @@ class ManageProducts extends StatefulWidget {
 }
 
 class _ManageProductsState extends State<ManageProducts> {
+  
+
+ File? csvfile;
+  dynamic users;
+
+  selectCSVFile() async {
+    final result = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: ['csv']);
+
+    if (result != null) {
+      final path = result.files.single.path!;
+      setState(() {
+        csvfile = File(path);
+        users = csvfile?.readAsStringSync();
+        List<User> users1 = csvToUsersList(users);
+
+        String dataBuffer = "";
+
+        for (var i = 0; i < users1.length; i++) {
+          dataBuffer += (i == 0) ? "" : ",\n";
+          dataBuffer += users1[i].toJson();
+        }
+        dataBuffer = "[" + dataBuffer + "]";
+
+        print(dataBuffer);
+    
+      });
+    }
+  }
+  
+  
+   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Manage Products")),
-       drawer:AdDrawer() ,
+      drawer: AdDrawer(),
       body: Container(
-        child: Column(
-          children: [
-            Container(
-              child: Row(children: [
+        child: Column(children: [
+          Container(
+            child: Row(
+              children: [
                 Text("Products"),
                 Spacer(),
                 ElevatedButton.icon(
-      onPressed: (){
-        print("You pressed Icon Elevated Button");
-         Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const CsvToJson()),
-              );
-      }, 
-      icon: Icon(Icons.save),  //icon data for elevated button
-      label: Text("Upload CSV"), //label text 
-    )
-                
-                ],),
+                  onPressed:selectCSVFile,
+                  icon: Icon(Icons.save), //icon data for elevated button
+                  label: Text("Upload CSV"), //label text
+                )
+              ],
             ),
-    
-    
+          ),
         ]),
       ),
     );
   }
 }
+
+class User {
+  String username;
+  String email;
+  String phone;
+
+  User(this.username, this.email, this.phone);
+  toJson() {
+    return '{ "username": "${username}", "email": "${email}", "phone": "${phone}" }';
+  }
+}
+
+List<User> csvToUsersList(String data) {
+  List<User> users = [];
+
+  List<String> userin = data.split("\n");
+
+  for (int i = 1; i < userin.length; i++) {
+    List<String> user = userin[i].split(",");
+
+    users.add(User(user[0], user[1], user[2]));
+  }
+
+  return users;
+}
+
+
+
+
+
+
+// Future<void> _dialogBuilder(BuildContext context) {
+  
+//     return showDialog<void>(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text('Note:'),
+//           content: const Text(' if you upload new csv file  it will remove privous data.'),
+//           actions: <Widget>[
+//             TextButton(
+//               style: TextButton.styleFrom(
+//                 textStyle: Theme.of(context).textTheme.labelLarge,
+//               ),
+//               child: const Text('Cancel'),
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//             TextButton(
+//               style: TextButton.styleFrom(
+//                 textStyle: Theme.of(context).textTheme.labelLarge,
+//               ),
+//               child: const Text('Enable'),
+//               onPressed: () {
+                
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+
