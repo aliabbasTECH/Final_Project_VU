a24b5a2136583fcba7d1f5bb8768756ab69e9ec1
aliabbastech03 <aliabbas.tech03@gmail.com>
2023-01-17 02:13:30 +0500

    1/17/2023

--- lib/component/singleprductpage.dart
+++ lib/component/singleprductpage.dart
@@ -1,14 +1,13 @@
-import 'dart:ffi';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
 import '../screens/cartPage.dart';
 
 class SingleProductPage extends StatefulWidget {
   final productData;
   final cruser;
   int price;
-  SingleProductPage({this.productData, this.cruser,  required this.price});
+  SingleProductPage({this.productData, this.cruser, required this.price});
 
   @override
   State<SingleProductPage> createState() => _SingleProductPageState();
@@ -17,32 +16,39 @@ class SingleProductPage extends StatefulWidget {
 class _SingleProductPageState extends State<SingleProductPage> {
   dynamic cartData;
   int _quantity = 1;
-
+   DateTime today = DateTime.now();
   void incrementQuantity() {
     setState(() {
       _quantity++;
-      widget.price += int.parse(widget.productData['price'])  ;
+      widget.price += int.parse(widget.productData['price']);
     });
   }
 
   decrementQuantity() {
     setState(() {
-      if (_quantity > 1 || widget.price > int.parse(widget.productData['price']) ) {
+      if (_quantity > 1 ||
+          widget.price > int.parse(widget.productData['price'])) {
         _quantity--;
-        widget.price -= int.parse(widget.productData['price'])  ;
+        widget.price -= int.parse(widget.productData['price']);
       }
     });
   }
 
-  NavTocartPage(e) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => CartPage(data: e)));
+  NavTocartPage(e) async {
+    var url =
+        "https://daily-groceries-db-default-rtdb.firebaseio.com/database/users/${widget.cruser[1]}/data.json";
+    var res = await http.post(Uri.parse(url), body: json.encode(e)).then(
+        await Navigator.push(context,
+            MaterialPageRoute(builder: (context) => CartPage(data: e))));
+  }
+
+  NavToPlaceorder(e) {
+    print(e);
   }
 
   @override
   Widget build(BuildContext context) {
-    print('Q===> $_quantity');
-    print('P===> ${widget.price}');
+   
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -60,45 +66,81 @@ class _SingleProductPageState extends State<SingleProductPage> {
                 style: Theme.of(context).textTheme.headline6,
               ),
               SizedBox(height: 10),
-              Row(
-                children: [
-                  Text(
-                    '\Rs${widget.price.toString()}',
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                  Spacer(),
-                  FloatingActionButton.small(
-                      child: Icon(
-                          const IconData(0xf2fe, fontFamily: 'MaterialIcons'),
-                          color: Colors.black),
-                      backgroundColor: Colors.white,
-                      onPressed: decrementQuantity),
-                  SizedBox(
-                      width: 30,
-                      child: Center(child: Text(_quantity.toString()))),
-                  FloatingActionButton.small(
-                      child: Icon(Icons.add, color: Colors.black87),
-                      backgroundColor: Colors.white,
-                      onPressed: incrementQuantity)
-                ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      '\Rs${widget.price.toString()}',
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    Spacer(),
+                    FloatingActionButton.small(
+                        child: Icon(
+                            const IconData(0xf2fe, fontFamily: 'MaterialIcons'),
+                            color: Colors.black),
+                        backgroundColor: Colors.white,
+                        onPressed: decrementQuantity),
+                    SizedBox(
+                        width: 30,
+                        child: Center(child: Text(_quantity.toString()))),
+                    FloatingActionButton.small(
+                        child: Icon(Icons.add, color: Colors.black87),
+                        backgroundColor: Colors.white,
+                        onPressed: incrementQuantity)
+                  ],
+                ),
               ),
-              SizedBox(height: 10),
               SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () => NavTocartPage(cartData = {
-                    "email": widget.cruser[0],
-                    "uuid": widget.cruser[1],
-                    "name": widget.productData["name"],
-                    "price": widget.price,
-                    "productId": widget.productData["productID"],
-                    "quantity": _quantity
-                  }),
-                  child: Text(
-                    'Add to Cart',
-                    style: TextStyle(color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  width: double.infinity,
+                  height: 60,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 140,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () => NavTocartPage(cartData = {
+                            "date": '${today.day}-${today.month}-${today.year}',
+                            "email": widget.cruser[0],
+                            "uuid": widget.cruser[1],
+                            "name": widget.productData["name"],
+                            "price": widget.price,
+                            "productId": widget.productData["productID"],
+                            "quantity": _quantity,
+                            "image":widget.productData["image1"]
+                          }),
+                          child: Text(
+                            'Add to Cart',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      SizedBox(
+                        width: 140,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () => NavToPlaceorder(cartData = {
+                            "date": '${today.day}-${today.month}-${today.year}',
+                            "email": widget.cruser[0],
+                            "uuid": widget.cruser[1],
+                            "name": widget.productData["name"],
+                            "price": widget.price,
+                            "productId": widget.productData["productID"],
+                            "quantity": _quantity,
+                            "image":widget.productData["image1"]
+                          }),
+                          child: Text(
+                            'Place Order',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
--- lib/screens/cartPage.dart
+++ lib/screens/cartPage.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
-  final data ;
+  final data;
   CartPage({this.data});
 
   @override
@@ -9,14 +12,60 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  dynamic url;
+  dynamic cartproducts;
+  dynamic keysdata;
+  Future getusersdata() async {
+    url =
+        "https://daily-groceries-db-default-rtdb.firebaseio.com/database/users/${widget.data['uuid']}/data.json";
+    final response = await http.get(Uri.parse(url));
+    setState(() {
+      dynamic resp = jsonDecode(response.body);
+      cartproducts = resp;
+      keysdata = cartproducts.keys.toList();
+    });
+    // data.map((e) => print(e[' name'])).toList();
+  }
 
- 
+  @override
+  void initState() {
+    super.initState();
+    this.getusersdata();
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(widget.data);
+    // print(keys);
+    print(keysdata);
+
     return Scaffold(
-      body: Center(child: Text("cart")),
+      appBar: AppBar(
+        title: Text("Cart"),
+      ),
+      body: ListView.builder(
+        itemCount: cartproducts.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: SizedBox(
+              width: 40,
+              child: Image.network(
+                        "${cartproducts[keysdata[index]]['image']}",
+                         fit: BoxFit.cover),
+            ),
+            title: Text("${cartproducts[keysdata[index]]['name']}"),
+            subtitle: Text("price : "),
+            trailing: IconButton(
+              icon: Icon(Icons.remove_shopping_cart),
+              onPressed: () {
+                setState(() {
+                  // _items.removeAt(index);
+                });
+              },
+            ),
+          );
+        },
+      ),
+     
     );
   }
 }
